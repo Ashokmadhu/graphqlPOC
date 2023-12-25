82ac9d6069cc1f7af580e6ef8194e97fec95930c
BASKARAN Ashokkumar <ashokkumar.baskaran@rntbci.com>
2023-12-25 13:47:10 +0530

    Added files

---
+++ src/test/java/com/nissan/pace/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.nissan.pace.graphql;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class GraphqlApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
